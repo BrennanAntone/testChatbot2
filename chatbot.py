bebcbd583dcc87e22dd3d2ca0d102d1ce7b7ddad
Brennan Antone <brennanantone@gmail.com>
2023-03-02 13:13:59 -0500

    Add streamlit requirements list

--- chatbot.py
+++ chatbot.py
@@ -9,8 +9,7 @@ import streamlit as st
 # import streamlit_chat
 from streamlit_chat import message
 
-openai.api_key = st.secrets["OPENAI_SECRET"]
-
+#openai.api_key = st.secrets["OPENAI_SECRET"]
 
 def generate_response(prompt):
     completions = openai.Completion.create(
